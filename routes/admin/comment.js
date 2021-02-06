3d72f04303565babb3b1f47742a96595c413c0b3
even <731788284@qq.com>
2021-02-06 23:18:49 +0800

    新增评论模块

---
+++ routes/admin/comment.js
@@ -0,0 +1,80 @@
+module.exports = (app, plugin, model, config) => {
+  const express = require('express');
+  const router = express.Router();
+
+  const { Comment, Counter, Article } = model
+  const { email, getPage, requestResult } = plugin
+
+  // 获取评论列表
+  router.get('/comment', async (req, res) => {
+    const p = req.query.page || 1;
+    const s = req.query.count || 10;
+
+    const data = await getPage(Comment, p, s)
+    res.send(requestResult(data))
+  })
+
+  // 删除评论
+  router.delete('/comment', async (req, res) => {
+    const id = req.body.id;
+    const total = [
+      new Promise((resolve, reject) => {
+        Comment.deleteOne({ id }, (err, doc) => doc ? resolve(doc) : reject())
+      })
+    ]
+    // 删除所有子评论
+    if (!req.body.parent_id) {
+      total.push(new Promise((resolve, reject) => {
+        Comment.deleteMany({ parent_id: id }, (err, doc) => doc ? resolve(doc) : reject())
+      }))
+    }
+    Promise.all(total).then(resolve => {
+      res.send(requestResult('删除成功', 0, resolve[0]))
+    }).catch(err => {
+      res.send(requestResult('删除失败', 1, err))
+    })
+  })
+  // Reply
+  router.post('/comment', async (req, res) => {
+    const commentCount = await Counter.findOneAndUpdate({
+      name: 'comment'
+    }, {
+      $inc: { 'count': 1 }
+    }, {
+      new: true
+    })
+
+    // 添加评论id
+    req.body.data.id = commentCount.count;
+    const result = await Comment.create(req.body.data)
+
+    res.send(requestResult('', 0, result))
+
+    // 邮件信息
+    if (req.body.email.comment) {
+      const articleData = await Article.findOne({ id: req.body.data.topic_id })
+      const data = {
+        title: articleData.title,
+        url: req.body.email.web_address + '/' + req.body.data.topic_id,
+        name: req.body.data.reply_name,
+        email: req.body.data.reply_email
+      }
+      // 发送邮件
+      email(3, data, req.body.email)
+    }
+  })
+
+  // 一键已读
+  router.post('/comment_read', async (req, res) => {
+    const comment = await Comment.updateMany({
+      status: 1
+    }, {
+      $set: { status: 2 }
+    }, {
+      multi: true
+    }, (err, doc) => {
+      return doc;
+    })
+  })
+  app.use('/admin/api', router)
+}
---
+++ routes/admin/info.js
@@ -0,0 +1,51 @@
+module.exports = (app, plugin, model, config) => {
+  const express = require('express');
+  const router = express.Router();
+
+  let { Info, Comment, Counter, Article, Envelope } = model
+  let { dateFormat, requestResult } = plugin
+
+  router.get('/info', async (req, res) => {
+    const result = await Promise.all([
+      Info.findOne(),
+      Article.findOne().sort({ time: -1 }),
+      Envelope.find().sort({ time: -1 }).limit(8),
+      Article.countDocuments(),
+      Comment.countDocuments(),
+      Comment.find({ status: 1 }).countDocuments()
+    ])
+    result[2].forEach(item => item._doc['time'] = dateFormat(item.time))
+
+    const key = [
+      'info',         // 个人信息
+      'article',      // 文章列表
+      'envelope',     // 短语列表
+      'articleCnt',   // 文章总数
+      'commentCnt',   // 评论总数量
+      'unReadCnt'        // 评论未读数
+    ]
+    const data = key.reduce((total, item, index) => {
+      total[item] = result[index]
+      return total
+    }, {})
+
+    res.send(requestResult('获取成功', 0, data))
+  })
+
+  router.post('/info', async (req, res) => {
+    if (req.body._id) {
+      const result = await Info.findByIdAndUpdate(
+        req.body._id,
+        req.body,
+        (err, doc) => {
+          return doc
+        })
+      res.send(requestResult('提交成功', 0, result))
+    } else {
+      const result = await Info.create(req.body)
+      res.send(requestResult('提交失败', 1, result))
+    }
+  })
+
+  app.use('/admin/api', router)
+}
